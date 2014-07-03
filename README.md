bd75b8c6501e897d0416f83f32be8bee2daac9a7
Kenan Sulayman <kenan@sly.mn>
2014-07-03 18:05:08 +0200

    Updates read, bumps version defucks package.json

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Install globally
 $ npm install adddep -g
 ```
 
-# Usage
+# Adding modules
+
 Add the latest verison of an npm package
 ```
 $ adddep jade
@@ -24,17 +25,19 @@ $ adddep jade socket.io
 
 Specify version
 
+Prefix *@* in order to force a version, otherwise the latest version is used. When specifying a version, mind the *v* prefixing the semver expression.
+
 ```
-$ adddep 'jade 0.30.0'
+$ adddep @jadev0.30.0
 ```
 
 ```
-$ adddep 'jade 0.30.0' 'socket.io 0.9.14'
+$ adddep '@jadev0.30.0' '@socket.iov0.30.0'
 ```
 
 Both
 ```
-$ addep express 'socket.io 0.9.14'
+$ addep express '@socket.iov0.9.14'
 ```
 
 Add and install
@@ -45,3 +48,23 @@ or
 ```
 $ adddep express stylus async -i
 ```
+
+# Removing modules
+```
+$ adddep -express -stylus
+```
+
+Mixed usage
+
+```
+$ adddep -express @jadev0.30.0
+```
+
+# Bumping a module version
+`adddep` provides a function which allows bumping the module version.
+
+```
+$ adddep -b 1.0.0
+```
+
+The expressions will automatically be expanded: 1 - 1.0.0; 1.1 - 1.1.0.
--- add.js
+++ add.js
@@ -15,10 +15,20 @@ var sign = [
 	   	"        / _ `/ _ \\\\ \\ /",
 	   	"        \\_,_/ .__/_\\_\\ ",
 	   	"           /_/\n\t\t\033[1mdepadd " + self.version, "\033[0m",
-	   	"\tCLI for adding deps to a npm module.",
+	   	"\tCLI for adding deps to a npm module."
 	].join("\n");
 
-var help = sign + '\n\nUsage: \n\tadddep <packages> [options]\n\nOptions:\n\t-h, --help\tHelp screen\n\t-v, --version\tCurrent version\n\t-i, --install\tinstall packages after adding them';
+var help = sign + [
+	"", "",
+	"Usage: ",
+	"\tadddep <packages> [options]", "", "",
+	"Options:",
+	"\t-h, --help\tHelp screen",
+	"\t-v, --version\tCurrent version",
+	"\t-i, --install\tInstall packages after adding them",
+	"\t-b, --bump\tUpdate the version of a module (valid semver must be specified)"
+].join("\n");
+
 var version = self.version;
 
 if (process.argv[0] == 'node') {
@@ -26,6 +36,14 @@ if (process.argv[0] == 'node') {
 } else {
 	argv = process.argv;
 }
+
+var log = function () {
+	arguments = Array.prototype.slice.call(arguments);
+	var bold = arguments[arguments.length - 1] === -0xA0; if(bold)arguments=arguments.slice(0, arguments.length-1);
+	process.stdout.write.call(process.stdout, (bold?"\033[1m":"")+(arguments.length ? arguments.concat(["\n"]) : ["\n"]).join(" ")+(bold?"\033[0m":""));
+};
+
+console.log = new Function;
 
 var exit = function (message, error) {
 	process.stdout.write(message + "\n\n");
@@ -37,6 +55,22 @@ var _install = function () {
 	npm.load({}, function(err) {
 		npm.commands.install([], function(err, rd) {})
 	})
+}
+
+// semver expand
+var sve = function (ver) {
+	var vsl = String(ver).split(".").length;
+
+	// 1 => 1.0.0
+	if ( vsl === 1 ) {
+		ver = ver + ".0.0";
+	} else
+	// 1.1 => 1.1.0
+	if ( vsl === 2 ) {
+		ver = ver + ".0";
+	}
+
+	return ver;
 }
 
 var base = process.cwd();
@@ -54,8 +88,8 @@ var main = function(deps, install) {
 		var _next = function () {
 			if ( ++i !== deps.length ) return;
 
-			fs.writeFileSync(base + 'package.json', JSON.stringify(package, null, 2));
-			console.log("\n» Updated package.json");
+			fs.writeFileSync(base + '/package.json', JSON.stringify(package, null, "\t"));
+			log("\n» Updated package.json\n");
 		}
 
 		deps.forEach(function (pck) {
@@ -70,7 +104,7 @@ var main = function(deps, install) {
 				var sx = pck.slice(lI + 1);
 
 				if (svr.test(sx)) {
-					ver = sx;
+					ver = sve(sx);
 					pck = pck.slice(0, lI)
 				}
 			}
@@ -80,31 +114,49 @@ var main = function(deps, install) {
 
 			if (fc) pck = pck.slice(1);
 
+			var del = pck[0] === "-";
+
+			if (del) pck = pck.slice(1);
+
 			package.dependencies = package.dependencies || [];
+
+			console.log("fc", fc, "wc", wc, "ver", ver, "del", "del")
+
+			if (del) {
+				if (package.dependencies[pck]) {
+					delete package.dependencies[pck];
+
+					log("» Deleting: " + pck);
+				} else {
+					return log("» Couldn't locate " + pck + " in package.. ignoring.", -0xA0);
+				}
+
+				return _next();
+			}
 
 			if ( fc ) {
 				package.dependencies[pck] = wc ? "*" : ver;
 
-				console.log("» Adding: ", pck + " v" + package.dependencies[pck])
+				log("» Adding: ", pck + " v" + package.dependencies[pck])
 
-				_next();
+				return _next();
 			} else {
 				npm.commands.show([pck, 'name'], function(err, mod) {
-					if (err) console.log("Err:", err);
+					if (err) return log("« Warning: couldn't locate " + pck + ".. omitting it.", -0xA0), _next();
 
 					// get top-most version
 					var version = wc ? "*" : Object.keys(mod)[0];
 
-					ver && console.log(
-						"\033[1m« Warning: Specified version " + ver + ", but didn't force it. (use @" + pck + "v" + ver + ")\n" +
-						"« Warning: Using upstream " + version + " instead.\033[0m"
-					);
+					ver && log(
+						"« Warning: Specified version " + ver + ", but didn't force it. (use @" + pck + "v" + ver + ")\n" +
+						"« Warning: Using upstream " + version + " instead."
+					, -0xA0);
 
 					package.dependencies[pck] = version;
 
-					console.log("» Adding: ", pck + " v" + package.dependencies[pck])
+					log("» Adding: ", pck + " v" + package.dependencies[pck])
 
-					_next();
+					return _next();
 				});
 			}
 		});
@@ -115,8 +167,15 @@ var main = function(deps, install) {
 
 var xa = [], xo = [];
 
+var ops = [
+	"-h", "--help",
+	"-v", "--version",
+	"-i", "--install",
+	"-b", "--bump"
+]
+
 argv.forEach(function (v) {
-	return v[0] === "-" ? xo.push(v) : xa.push(v);
+	return ~ops.indexOf(v) ? xo.push(v) : xa.push(v);
 })
 
 if (require.main === module) {
@@ -138,8 +197,42 @@ if (require.main === module) {
 		}
 
 		if ( xo[0] === "-b" || xo[0] === "-bump" ) {
-			if (svr.test(xa[0])) {
+			log(sign);
 
+			if (svr.test(xa[0])) {
+				var ver = sve(xa[0]);
+
+				try {
+					var package = JSON.parse(fs.readFileSync(base + "/package.json"));
+				} catch(e) {
+					return exit("\n« Warning: Bump failed, inexistent or malformed package.json.");
+				}
+
+				if ( package.version === ver ) {
+					log(
+						"\n« Bumping: Version didn't change, aborting.\n"
+					, -0xA0);
+
+					return process.reallyExit();
+				} else {
+					log(
+						"\n« Bumping: Updating from " + package.version + " to " + ver
+					, -0xA0);
+
+					package.version = ver;
+
+					fs.writeFileSync(base + '/package.json', JSON.stringify(package, null, "\t"));
+					log("\n» Updated package.json");
+
+					return process.reallyExit();
+				}
+
+			} else {
+				log(
+					"\n\t« Warning: Trying to bump version but specified version is invalid. (" + (xa[0] ? xa[0] : "none given") + ")\n"
+				, -0xA0);
+
+				return process.reallyExit();
 			}
 		}
 	})
@@ -147,7 +240,7 @@ if (require.main === module) {
 	if (!xa.length && !xo.length) {
 		exit(help)
 	} else {
-		console.log(sign)
+		log(sign + "\n")
 	}
 
 	!stop && main(xa, install);
